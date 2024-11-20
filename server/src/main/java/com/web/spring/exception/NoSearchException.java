d6fa5de8ea6d673fed2174c27f435f12456bfdd9
skyblue1012 <thwn1012@gmail.com>
2024-11-20 17:44:50 +0900

    [FE]fix : index.js 파일 수정

--- server/src/main/java/com/web/spring/exception/NoSearchException.java
+++ server/src/main/java/com/web/spring/exception/NoSearchException.java
@@ -5,18 +5,18 @@ import org.springframework.http.HttpStatus;
 import lombok.Getter;
 
 @Getter
-public class BoardSearchNotException extends RuntimeException{
+public class NoSearchException extends RuntimeException{
 	private String message;
 	private HttpStatus httpStatus;
 	private String title;
 	
-	public BoardSearchNotException(String message, String title,HttpStatus httpStatus) {	
+	public NoSearchException(String message, String title,HttpStatus httpStatus) {	
 		this.message = message;
 		this.httpStatus = httpStatus;
 		this.title = title;
 	}
 	
-	public BoardSearchNotException(String message, String title) {	
+	public NoSearchException(String message, String title) {	
 		this(message,title,HttpStatus.EXPECTATION_FAILED);
 	}
 }
